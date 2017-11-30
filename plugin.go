212a894e190dc5b2692389f0ea327acb66614295
Maciej Adwent <Maciek416@users.noreply.github.com>
2017-11-30 14:25:46 -0800

    Add sleep

--- plugin.go
+++ plugin.go
@@ -16,11 +16,7 @@ type Plugin struct {
 }
 
 func (p Plugin) Exec() error {
-	const sleepytime = 10 * time.Second
-
-	fmt.Printf("Sleeping for %v before performing fetch...\n\n", sleepytime)
-	time.Sleep(sleepytime)
-	fmt.Printf("Done sleeping.\n\n")
+	time.Sleep(time.Second * 10)
 
 	if p.Build.Path != "" {
 		err := os.MkdirAll(p.Build.Path, 0777)
